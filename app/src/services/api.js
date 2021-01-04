ca5065e2b0b3f2000160aadbb5ee06a18b4c413f
LeandroKrygoskii <53976591+LeandroKrygoskii@users.noreply.github.com>
2021-01-04 12:38:58 -0300

    update api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -9,7 +9,7 @@ const checkToken = async (token) => {
     },
     //body : JSON.stringify({token})
   }
-  const req = await fetch('http://192.168.15.6:3333/auth/refresh', requestOptions)
+  const req = await fetch('http://localhost:3333/auth/refresh', requestOptions)
 
   const data = await req.json();
 
@@ -24,7 +24,7 @@ const cadastro= async (name, email, password) => {
       headers : {'Content-Type': 'application/json'},
       body: JSON.stringify({name, email , password})
     }
-    const req = await fetch('http://192.168.15.6:3333/cadastro' , requestOptions)
+    const req = await fetch('http://localhost:3333/cadastro' , requestOptions)
   
     const data = await req.json();
 
@@ -41,7 +41,7 @@ const cadastro= async (name, email, password) => {
        body : JSON.stringify({email, password})
      }
     
-     const req = await fetch('http://192.168.15.6:3333/auth', requestOptions)
+     const req = await fetch('http://localhost:3333/auth', requestOptions)
     
      const data = await req.json();
 
